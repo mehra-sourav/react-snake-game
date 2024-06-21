ab8468edff1dc4af8d506acefde37e4c36d5ae1a
Sourav <1997mehra@gmail.com>
2024-06-21 18:26:28 +0530

    add handler for changing snake direction

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
-import { GameBoardContainer, SnakeSegment, Food } from "./GameBoard.styles";
+import { useState, useEffect } from "react";
+import { KEY_DIRECTION_MAP, OPPOSITE_DIRECTION_MAP } from "@/constants";
+import {
+  GameBoardContainer,
+  SnakeSegment,
+  FoodPellet,
+} from "./GameBoard.styles";
 
 const GameBoard = () => {
   const [snakePosition, setSnakePosition] = useState([{ x: 2, y: 2 }]); // Initial snake position
   const [foodPosition, setFoodPosition] = useState({ x: 5, y: 5 }); // Initial food pellet position
   const [snakeDirection, setSnakeDirection] = useState("RIGHT"); // Initial snake direction
 
+  // Side effect for changing snake direction
+  useEffect(() => {
+    window.addEventListener("keydown", changeSnakeDirection);
+    return () => window.removeEventListener("keydown", changeSnakeDirection);
+  }, [snakeDirection]);
+
+  const changeSnakeDirection = (event) => {
+    const newDirection = KEY_DIRECTION_MAP[event.keyCode];
+    if (
+      newDirection &&
+      newDirection !== OPPOSITE_DIRECTION_MAP[snakeDirection]
+    ) {
+      setSnakeDirection(newDirection);
+    }
+  };
+
   return (
     <GameBoardContainer>
+      {snakeDirection}
       {snakePosition.map((segment, index) => (
         <SnakeSegment
           key={index}
           style={{ gridRowStart: segment.y, gridColumnStart: segment.x }}
         />
       ))}
-      <Food
+      <FoodPellet
         style={{
           gridRowStart: foodPosition.y,
           gridColumnStart: foodPosition.x,
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,13 @@
 export const BOARD_SIZE = 15;
+export const KEY_DIRECTION_MAP = {
+  37: "LEFT",
+  38: "UP",
+  39: "RIGHT",
+  40: "DOWN",
+};
+export const OPPOSITE_DIRECTION_MAP = {
+  UP: "DOWN",
+  DOWN: "UP",
+  LEFT: "RIGHT",
+  RIGHT: "LEFT",
+};
