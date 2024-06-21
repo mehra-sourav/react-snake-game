1fdd4099568fe864f378a434f3f705fa85e5c025
Sourav <1997mehra@gmail.com>
2024-06-21 18:12:07 +0530

    feat: add gameboard component and absolute paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import Gameboard from "@/components/GameBoard";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Gameboard</>;
+  return <Gameboard />;
 }
 
 export default App;
