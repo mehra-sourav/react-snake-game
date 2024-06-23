a67d30e72ec6387d34a56e580da1614bd9864e8a
Sourav <1997mehra@gmail.com>
2024-06-23 18:15:30 +0530

    feat: add game over check

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   KEY_DIRECTION_MAP,
   OPPOSITE_DIRECTION_MAP,
@@ -13,11 +13,12 @@ import {
 
 const GameBoard = () => {
   const [snakePosition, setSnakePosition] = useState([
-    { x: 2, y: 2 },
     { x: 3, y: 2 },
+    { x: 2, y: 2 },
   ]); // Initial snake position
   const [foodPosition, setFoodPosition] = useState({ x: 5, y: 5 }); // Initial food pellet position
   const [snakeDirection, setSnakeDirection] = useState("RIGHT"); // Initial snake direction
+  const snakeMoveIntervalRef = useRef(null);
 
   // Side effect for changing snake direction
   useEffect(() => {
@@ -28,8 +29,9 @@ const GameBoard = () => {
   // Side effect for moving snake
   useEffect(() => {
     const interval = setInterval(moveSnake, SNAKE_RATE_OF_CHANGE);
+    snakeMoveIntervalRef.current = interval;
     return () => clearInterval(interval);
-  }, [snakeDirection]);
+  }, [snakeDirection, snakePosition]);
 
   const changeSnakeDirection = (event) => {
     const newDirection = KEY_DIRECTION_MAP[event.keyCode];
@@ -42,35 +44,32 @@ const GameBoard = () => {
   };
 
   const moveSnake = () => {
-    setSnakePosition((prevSnake) => {
-      const newHead = { ...prevSnake[0] };
-      switch (snakeDirection) {
-        case "RIGHT":
-          newHead.x += 1;
-          break;
-        case "LEFT":
-          newHead.x -= 1;
-          break;
-        case "DOWN":
-          newHead.y += 1;
-          break;
-        case "UP":
-          newHead.y -= 1;
-          break;
-        default:
-          break;
-      }
+    const snakeNextPosition = getSnakeNextPosition();
+    const isWallCollision = checkWallCollision(snakeNextPosition[0]);
+    const isSelfCollision = checkSelfCollision(
+      snakeNextPosition[0],
+      snakeNextPosition.slice(1)
+    );
 
-      const isFoodEatem = isSnakeOnFood(newHead);
+    // Continue game if snake hasn't collided with itself or any wall
+    if (!isWallCollision && !isSelfCollision) {
+      setSnakePosition((prevSnake) => {
+        const [newHead, ...newBody] = snakeNextPosition;
+        const isFoodEaten = isSnakeOnFood(newHead);
 
-      if (isFoodEatem) {
-        let newFoodPosition = generateFoodPosition();
-        setFoodPosition(newFoodPosition);
-        return [newHead, ...prevSnake];
-      }
+        if (isFoodEaten) {
+          let newFoodPosition = generateFoodPosition();
+          setFoodPosition(newFoodPosition);
+          return [newHead, ...snakePosition];
+        }
 
-      return [newHead, ...prevSnake.slice(0, -1)];
-    });
+        return snakeNextPosition;
+      });
+    }
+    // Game over
+    else {
+      clearInterval(snakeMoveIntervalRef.current);
+    }
   };
 
   //   Checks if snake's head is on the same coordinates as food
@@ -85,6 +84,45 @@ const GameBoard = () => {
       y: Math.floor(Math.random() * BOARD_SIZE) + 1,
     };
     return newFoodPosition;
+  };
+
+  // Returns the next position of snake
+  const getSnakeNextPosition = () => {
+    const newHead = { ...snakePosition[0] };
+
+    switch (snakeDirection) {
+      case "RIGHT":
+        newHead.x += 1;
+        break;
+      case "LEFT":
+        newHead.x -= 1;
+        break;
+      case "DOWN":
+        newHead.y += 1;
+        break;
+      case "UP":
+        newHead.y -= 1;
+        break;
+      default:
+        break;
+    }
+
+    return [newHead, ...snakePosition.slice(0, -1)];
+  };
+
+  // Checks if snake has collided with the walls
+  const checkWallCollision = (snakeHeadPosition) => {
+    const { x, y } = snakeHeadPosition;
+    return x <= 0 || y <= 0 || x > BOARD_SIZE || y > BOARD_SIZE;
+  };
+
+  // Checks if snake has collided with its own body
+  const checkSelfCollision = (snakeHeadPosition, snakeBodyPosition) => {
+    const { x, y } = snakeHeadPosition;
+
+    return snakeBodyPosition.some(
+      ({ x: bodyX, y: bodyY }) => bodyX === x && bodyY === y
+    );
   };
 
   return (
